45599b760f55572265b20c5ac182798cece94005
Rob Rimmer <developer@atrico.net>
2014-08-07 00:08:21 +0100

    Version corrected
    Version had been increased correctly
    Now 1.1.0.0 (consistent with semantic versioning - http://semver.org/)

--- Atrico.Lib.Assertions.Test/Properties/AssemblyInfo.cs
+++ Atrico.Lib.Assertions.Test/Properties/AssemblyInfo.cs
@@ -3,4 +3,3 @@
 [assembly: AssemblyTitle("Atrico.Lib.Assertions.Test")]
 [assembly: AssemblyProduct("Atrico.Lib.Assertions.Test")]
 [assembly: AssemblyDescription("Tests for Atrico.Lib.Assertions")]
-[assembly: AssemblyVersion("1.0.0.0")]
--- Atrico.Lib.Assertions/Properties/AssemblyInfo.cs
+++ Atrico.Lib.Assertions/Properties/AssemblyInfo.cs
@@ -3,5 +3,3 @@
 [assembly: AssemblyTitle("Atrico.Lib.Assertions")]
 [assembly: AssemblyProduct("Atrico.Lib.Assertions")]
 [assembly: AssemblyDescription("Assertion constraint for use like: Assert.That(item, constraint)")]
-
-[assembly: AssemblyVersion("1.0.0.3")]
